e8cb0c3d4a95400ef7806508c64be5ae015b2b6d
Maulana Ghozi Humama <ghozihz@gmail.com>
2019-07-02 11:43:37 +0700

    add file string.js in directory exercise 5

---
+++ Phase0/Week2/Anchor/Rabu/Exercise5/string.js
@@ -0,0 +1,14 @@
+// Number 1
+// make a sentence "JavaScript is awesome and I love it!""
+
+var word = 'JavaScript';
+var second = 'is';
+var third = 'awesome';
+var fourth = 'and';
+var fifth = 'I';
+var sixth = 'love';
+var seventh = 'it!';
+
+console.log(word+' '+second+' '+third+' '+fourth+' '+fifth+' '+sixth+' '+seventh);
+
+// Number 2
